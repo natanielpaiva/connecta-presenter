2c8976425b520c39d8af3992b8db0560ef0cf535
Nataniel Freire de Paiva <nataniel.paiva@gmail.com>
2015-07-15 20:57:16 +0000

    FInalizando o primeiro visualizador

--- src/main/java/br/com/cds/connecta/presenter/controller/AnalysisViewerController.java
+++ src/main/java/br/com/cds/connecta/presenter/controller/AnalysisViewerController.java
@@ -9,9 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,36 +23,49 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 @RequestMapping("analysis-viewer")
-public class AnalysisViewerController extends AbstractBaseController<AnalysisViewer> {
+public class AnalysisViewerController{
 
     @Autowired
     private IAnalysisViewerAS analysisViewerService;
-    
+
     @Autowired
     private IDataExtractorAS dataExtratorService;
 
     @RequestMapping(value = "result/{id}", method = RequestMethod.GET)
-    public ResponseEntity<AnalysisViewerResult> getResult(@PathVariable("id") Long id){
+    public ResponseEntity<AnalysisViewerResult> getResult(@PathVariable("id") Long id) {
         AnalysisViewer analysisViewer = analysisViewerService.get(id);
         AnalysisViewerResult analysisViewerResult = dataExtratorService.getAnalysisViewerResult(analysisViewer);
         return new ResponseEntity<>(analysisViewerResult, HttpStatus.OK);
     }
 
-    @Override
-    protected ResponseEntity<AnalysisViewer> save(AnalysisViewer analysisViewer,
-            HttpServletRequest request, HttpServletResponse response) throws Exception {
-        AnalysisViewer analysisViewerResult = analysisViewerService.saveOrUpdate(analysisViewer);
+    @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+                    method = RequestMethod.POST)
+    protected ResponseEntity<AnalysisViewerResult> save(@RequestBody AnalysisViewer analysisViewer) 
+            throws Exception {
+        AnalysisViewer analysisViewerResultNew = analysisViewerService.saveOrUpdate(analysisViewer);
+        AnalysisViewerResult analysisViewerResult = dataExtratorService.getAnalysisViewerResult(analysisViewerResultNew);
+
         return new ResponseEntity<>(analysisViewerResult, HttpStatus.CREATED);
     }
+    
+    @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+                    method = RequestMethod.POST,
+                    value = "preview")
+    protected ResponseEntity<AnalysisViewerResult> getPreview(@RequestBody AnalysisViewer analysisViewer) 
+            throws Exception {
+        AnalysisViewerResult analysisViewerResult = dataExtratorService.getAnalysisViewerResult(analysisViewer);
 
-    @Override
+        return new ResponseEntity<>(analysisViewerResult, HttpStatus.OK);
+    }
+
+   
     protected ResponseEntity<AnalysisViewer> update(Long id, AnalysisViewer analysisViewer,
             HttpServletRequest request, HttpServletResponse response) throws Exception {
         AnalysisViewer analysisViewerResult = analysisViewerService.saveOrUpdate(analysisViewer);
         return new ResponseEntity<>(analysisViewerResult, HttpStatus.OK);
     }
 
-    @Override
+    
     protected void delete(Long id, HttpServletRequest request, HttpServletResponse response) throws Exception {
         analysisViewerService.delete(id);
     }
