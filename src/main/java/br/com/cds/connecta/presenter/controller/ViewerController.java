05038205b8d4ac66be3f4a70b3a69b8f9ef1fd5c
Nataniel Freire de Paiva <nataniel.paiva@gmail.com>
2015-10-01 18:23:55 +0000

    Ajustando os viewer de amchart frontend

--- src/main/java/br/com/cds/connecta/presenter/controller/ViewerController.java
+++ src/main/java/br/com/cds/connecta/presenter/controller/ViewerController.java
@@ -4,7 +4,10 @@ import br.com.cds.connecta.framework.amcharts.ChartTemplate;
 import br.com.cds.connecta.framework.amcharts.ChartTemplateType;
 import br.com.cds.connecta.framework.amcharts.provider.ChartProvider;
 import br.com.cds.connecta.framework.core.controller.AbstractBaseController;
+import br.com.cds.connecta.presenter.bean.analysisviewer.AnalysisViewerResult;
 import br.com.cds.connecta.presenter.business.applicationService.IViewerAS;
+import br.com.cds.connecta.presenter.business.applicationService.dataExtractor.IDataExtractorAS;
+import br.com.cds.connecta.presenter.entity.viewer.AnalysisViewer;
 import br.com.cds.connecta.presenter.entity.viewer.Viewer;
 import java.io.IOException;
 import java.util.Collection;
@@ -16,8 +19,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -30,6 +36,9 @@ public class ViewerController extends AbstractBaseController<Viewer> {
 
     @Autowired
     private IViewerAS viewerService;
+    
+    @Autowired
+    private IDataExtractorAS dataExtratorService;
     
     private final ChartProvider chartProvider = new ChartProvider();
 
@@ -64,6 +73,18 @@ public class ViewerController extends AbstractBaseController<Viewer> {
     @Override
     protected void delete(Long id, HttpServletRequest request, HttpServletResponse response) throws Exception {
         viewerService.delete(id);
+    }
+    
+    @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+            method = RequestMethod.POST,
+            value = "preview")
+    @ResponseBody
+    public ResponseEntity<AnalysisViewerResult> preview(@RequestBody AnalysisViewer analysisViewer,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        AnalysisViewerResult analysisViewerResult = dataExtratorService.getAnalysisViewerResult(analysisViewer);
+
+        return new ResponseEntity<>(analysisViewerResult, HttpStatus.OK);
     }
     
     @RequestMapping(value = "chart-template", method = RequestMethod.GET,
