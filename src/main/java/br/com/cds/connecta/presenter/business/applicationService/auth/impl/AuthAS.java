8107fd96d4f8eeeccac5a36db9a1c369e700ba7f
diego.rego <diegofernandesrego@users.noreply.github.com>
2016-05-16 22:04:33 +0000

    Colocando possibilidade de rodar no H2, finalizando funcionalidade de valores possíveis para os filtros.

--- src/main/java/br/com/cds/connecta/presenter/business/applicationService/auth/impl/AuthAS.java
+++ src/main/java/br/com/cds/connecta/presenter/business/applicationService/auth/impl/AuthAS.java
@@ -13,30 +13,31 @@ import br.com.cds.connecta.presenter.business.applicationService.auth.IAuthAS;
 
 @Service
 public class AuthAS implements IAuthAS {
-	
-	private static final Logger logger = LoggerFactory.getLogger(AuthAS.class);
-	
-	private static final String SERVER_URL = "http://connectad.cds.com.br/connecta-portal/";
-	private static final String ENDPOINT_URL = "user/validarToken";
 
-	public boolean validateToken(String token){
-		
-		try {
-			HttpURLConnection urlCon = 
-					(HttpURLConnection) new URL(SERVER_URL+ENDPOINT_URL).openConnection();
-			urlCon.addRequestProperty("Authorization", token);
-			
-			int status = urlCon.getResponseCode();
-			
-			if(status != HttpStatus.OK.value()){
-				return false;
-			}
-		} catch (IOException e) {
-			logger.error("Não foi possível se conectar ao Portal");
-			return false;
-		}
-		
-		return true;
-	}
+    private static final Logger logger = LoggerFactory.getLogger(AuthAS.class);
+
+    private static final String SERVER_URL = "http://localhost:8080/connecta-portal/";
+    private static final String ENDPOINT_URL = "user/validarToken";
+
+    @Override
+    public boolean validateToken(String token) {
+
+        try {
+            HttpURLConnection urlCon
+                    = (HttpURLConnection) new URL(SERVER_URL + ENDPOINT_URL).openConnection();
+            urlCon.addRequestProperty("Authorization", token);
+
+            int status = urlCon.getResponseCode();
+
+            if (status != HttpStatus.OK.value()) {
+                return false;
+            }
+        } catch (IOException e) {
+            logger.error("Não foi possível se conectar ao Portal");
+            return false;
+        }
+
+        return true;
+    }
 
 }
