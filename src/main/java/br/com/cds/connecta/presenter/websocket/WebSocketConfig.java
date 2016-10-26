351f94686a32bcb79a734e41bbf048cdc035eff1
Cleto Gadêlha <cletogadelha@gmail.com>
2016-10-26 11:44:26 +0000

    migração wildfly 9.0.1.Final e websocket

--- src/main/java/br/com/cds/connecta/presenter/websocket/WebSocketConfig.java
+++ src/main/java/br/com/cds/connecta/presenter/websocket/WebSocketConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class AppWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 	
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
