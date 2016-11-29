6bb8f785bdbb32c827f69f46e48dc2828623821d
diego.rego <diegofernandesrego@users.noreply.github.com>
2016-11-29 18:43:35 +0000

    basic authentication

--- src/main/java/br/com/cds/connecta/presenter/business/strategy/connector/RestConnectorStrategy.java
+++ src/main/java/br/com/cds/connecta/presenter/business/strategy/connector/RestConnectorStrategy.java
@@ -9,12 +9,14 @@ import br.com.cds.connecta.framework.connector2.context.http.HttpResponse;
 import br.com.cds.connecta.framework.connector2.query.QueryBuilder;
 import br.com.cds.connecta.presenter.bean.analysis.AnalysisExecuteRequest;
 import br.com.cds.connecta.presenter.entity.analysis.RestAnalysis;
+import br.com.cds.connecta.presenter.entity.analysis.RestRequestVariableAnalysis;
 import br.com.cds.connecta.presenter.entity.datasource.RestDatasourceRequest;
 import br.com.cds.connecta.presenter.entity.datasource.RestRequestHeaderDatasource;
 import br.com.cds.connecta.presenter.persistence.RestAnalysisRepository;
 import br.com.cds.connecta.presenter.persistence.RestDatasourceRequestRepository;
 import br.com.cds.connecta.presenter.persistence.specification.RestAnalysisSpecification;
 import br.com.cds.connecta.presenter.persistence.specification.RestDatasourceRequestSpecification;
+import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -48,9 +50,12 @@ public class RestConnectorStrategy extends AbstractConnectorStrategy {
             datasourceRequest = requestRepository.findOne(RestDatasourceRequestSpecification.byIdFetch(restAnalysis.getRequest().getId()));
         } else {
             RestAnalysis findOne = restAnalysisRepository.findOne(RestAnalysisSpecification.getRestAnalyis(restAnalysis.getId()));
-             datasourceRequest = requestRepository.findOne(RestDatasourceRequestSpecification.byIdFetch(findOne.getRequest().getId()));
+            datasourceRequest = requestRepository.findOne(RestDatasourceRequestSpecification.byIdFetch(findOne.getRequest().getId()));
+            restAnalysis.setRequestVariables(findOne.getRequestVariables());
         }
 
+        datasourceRequest = factoryVariables(datasourceRequest, restAnalysis.getRequestVariables());
+        
         List<Header> headers = new ArrayList<>();
         for (RestRequestHeaderDatasource requestHeader : datasourceRequest.getHeaders()) {
             Header header = new Header();
@@ -58,8 +63,8 @@ public class RestConnectorStrategy extends AbstractConnectorStrategy {
             header.setValue(requestHeader.getValue());
             headers.add(header);
         }
+        
         HttpDataContextFactory httpDataContext = new HttpDataContextFactory();
-
         HttpResponse executeHttp = httpDataContext.executeHttp(
                 datasourceRequest.getUrl(),
                 datasourceRequest.getVerb(),
@@ -78,5 +83,17 @@ public class RestConnectorStrategy extends AbstractConnectorStrategy {
 
         return new Request(contextFactory, query);
 
+    }
+    
+    private RestDatasourceRequest factoryVariables(RestDatasourceRequest request, List<RestRequestVariableAnalysis> variables) {
+        Gson gson = new Gson();
+        String toJson = gson.toJson(request);
+
+        if (variables != null) {
+            for (RestRequestVariableAnalysis variable : variables) {
+                toJson = toJson.replace(variable.getVariable().getName(), variable.getValue());
+            }
+        }
+        return  gson.fromJson(toJson, RestDatasourceRequest.class);
     }
 }
